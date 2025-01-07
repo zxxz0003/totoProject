a88fe402a086ee20f1d9e3a411809606d5e26f34
zxxz0003 <g9512323@gmail.com>
2025-01-07 09:45:59 +0800

    000

--- 000/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({ 
-  base:process.env.NODE_ENV ==='production'? '/totoProject/' :'/',
-//base:'./',
-  plugins: [react()],
-})
